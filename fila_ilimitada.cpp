70bbd37176db0aec12882ef86dd687f090a4b430
Marcio-Renan <144846050+Marcio-Renan@users.noreply.github.com>
2024-04-13 15:03:31 -0300

    Update fila_ilimitada.cpp

--- fila_ilimitada.cpp
+++ fila_ilimitada.cpp
@@ -2,65 +2,66 @@
 
 using namespace std;
 
-template<typename TX>
-struct Fila{
-    int qtd_elem;
-    int tam;
-    int ultimo;
-    TX *vet;
+template<typename T>
+class Fila{
+    T *vet;
+    int size;
+    int last;
+    int qtt;
+    
+    public:
+        Fila(){
+            size = 1;
+            last = -1;
+            qtt = 0;
+            vet = new T[size];
+        }
+        
+        T primeiro(){
+            return vet[last - (qtt - 1)];
+        }
+        
+        void inserir(T element){
+            if(qtt == size){
+                T *aux = new T[size * 2];
+                int first = last - (qtt - 1);
+                for(int i = first; i < size; i++){
+                    aux[i] = vet[i];
+                }
+                delete vet;
+                *vet = *aux;
+            }
+            
+            vet[++last] = element;
+            qtt++;
+        }
+        
+        void remover(){
+            if(qtt == size/2){
+                T *aux = new T[size / 2];
+                int first = last - (qtt - 1);
+                for(int i = first; i < size; i++){
+                    aux[i] = vet[i];
+                }
+                delete vet;
+                *vet = *aux;
+            }
+            
+            qtt--;
+        }
 };
 
-template<typename TX>
-void inicializar(Fila<TX> &f){
-    f.ultimo = -1;
-    f.vet = new TX[1];
-    f.tam = 1;
-}
-
-template<typename TX>
-bool cheio(Fila<TX> &f){
-    return !((f.qtd_elem + 1) % f.tam);
-}
-
-template<typename TX>
-void inserir(Fila<TX> &f, TX elem){
-    if(cheio(f)){
-        TX *aux = new TX[2*f.tam];
-        for(int i = (f.ultimo + f.tam - f.qtd_elem + 1); i < f.tam; i++){
-            aux[i] = f.vet[i];
-        }
-        f.vet = aux;
-        f.tam *= 2;
-        delete f.vet;
+int main()
+{
+    Fila<int> fila;
+    fila.inserir(50);
+    fila.inserir(20);
+    fila.inserir(30);
+    fila.inserir(40);
+    for(int i = 0; i < 3; i++){
+        fila.remover();
     }
-    
-    f.vet[++f.ultimo] = elem;
-}
+    cout << fila.primeiro();
 
-template<typename TX>
-void remover(Fila<TX> &f){
-    f.qtd_elem--;
-    if(f.tam/2 == f.qtd_elem){
-        TX *aux = new TX[f.tam/2];
-        for(int i = (f.ultimo + f.tam - f.qtd_elem + 1); i < f.tam; i++){
-            aux[i] = f.vet[i];
-        }
-        f.vet = aux;
-        f.tam /= 2;
-        delete f.vet;
-    }
-}
-
-template<typename TX>
-TX primeiro(Fila<TX> &f){
-    return (f.ultimo + f.tam - f.qtd_elem + 1);
-}
-
-int main(){
-    Fila<int> f;
-    inicializar(f);
-    inserir(f, 5);
-    cout << primeiro(f);
-    
     return 0;
 }
